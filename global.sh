bdc0976f3db697541d74d6a00f2130efc57e3b22
Matthew Davies <daviesgeek@icloud.com>
2014-01-25 15:46:29 -0800

    Added color definitions Added section headers

--- global.sh
+++ global.sh
@@ -4,8 +4,20 @@
 # Matthew Davies, January 16th, 2014
 #
 ######
+
+# Variables
 APATH=$(pwd -P)
 NAME='TextTaskTic'
 DEFAULTCHECK='5'
 DEFAULTLOC=${HOME}/Dropbox/${NAME}
 SETTINGS=${HOME}/.$NAME/settings.cfg
+
+# Color definitions
+BLACK='\e[1;30m'
+BLUE='\e[1;34m'
+GREEN='\e[1;32m'
+CYAN='\e[1;36m'
+RED='\e[1;31m'
+PURPLE='\e[1;35m'
+YELLOW='\e[1;33m'
+NC='\e[0m'
