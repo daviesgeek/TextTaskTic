11862781c5619a820f701c444b23a6d36b61eb83
Matthew Davies <daviesgeek@icloud.com>
2014-01-25 00:07:18 -0800

    Added help.sh for usage information

---
+++ help.sh
@@ -0,0 +1,13 @@
+######
+#
+# Help info stuff
+# Matthew Davies, January 25th, 2014
+#
+######
+echo -e "Usage for $NAME [OPTIONS]...\n
+Option		Long Option	Meaning
+-h, -?		--help		Echoes help (this message) and exits
+-r		--reset		Reset
+-q		--quit		Quit
+-1		--once		(the number one) Runs $NAME once and exits
+no options 	Starts $NAME in the background"
