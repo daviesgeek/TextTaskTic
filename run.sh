4f7785b448fc4bcfe984dfb91b99512d360e6fff
Matthew Davies <daviesgeek@icloud.com>
2014-01-25 16:56:02 -0800

    Preliminary parsing. Still needs work, but the basics are there

--- run.sh
+++ run.sh
@@ -9,13 +9,23 @@ while true; do
 	files=( "$location"/* )
 	files=( "${files[@]##*/}" )
 
-	x=1
 	for f in "${files[@]}"; do
+		echo $f '=>'
 		readarray text < $location/$f
+		x=1
 		for l in "${text[@]}"; do
-			echo $l
-		done 
-		x=$((x+1))
+			heading='^[[:space:]]?#'
+			task='^[[:space:]]?-'
+			note='^[[:space:]]'
+			if [[ $l =~ $heading ]]; then
+				echo -e "$BLUE$l $NC"
+			elif [[ $l =~ $task ]]; then
+				echo -e "$RED$l $NC"
+			elif [[ $l =~ $note ]];then
+				echo -e "$l"
+			fi
+			x=$((x+1))
+		done
 	done
 
 	sleep $((check*60))
