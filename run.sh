b1c13d24b5de22ce25fb9e7456af842ea9794c13
Matthew Davies <daviesgeek@icloud.com>
2014-01-26 15:53:58 -0800

    Finished regex parsing Basic task parsing is done, now returns all outstanding tasks

--- run.sh
+++ run.sh
@@ -10,21 +10,56 @@ while true; do
 	files=( "${files[@]##*/}" )
 
 	for f in "${files[@]}"; do
-		echo $f '=>'
 		readarray text < $location/$f
-		x=1
+		filekey=1
 		for l in "${text[@]}"; do
 			heading='^[[:space:]]?#'
 			task='^[[:space:]]?-'
 			note='^[[:space:]]'
-			if [[ $l =~ $heading ]]; then
-				echo -e "$BLUE$l $NC"
-			elif [[ $l =~ $task ]]; then
-				echo -e "$RED$l $NC"
-			elif [[ $l =~ $note ]];then
-				echo -e "$l"
+			if [[ $l =~ $task ]]; then
+				datetimeregex='@\d{2,4}-\d{1,2}-\d{1,2},\d{1,2}:\d{1,2}'
+				datetime=$( echo $l | grep -oP $datetimeregex)
+				datesave="$datetime"
+
+				IFS=',' read -a datetime <<< "$datetime"
+				IFS='-' read -a date <<< "${datetime[0]}"
+				year=${date[0]/@/}
+				month=${date[1]}
+				day=${date[2]}
+
+				regex='^[0-9][0-9]$'
+				if [[ $year =~ $regex ]]; then
+					year="20$year"
+				fi
+				regex='^[0-9]$'
+				if [[ $month =~ $regex ]]; then
+					month="0$month"
+				fi
+				if [[ $day =~ $regex ]]; then
+					day="0$day"
+				fi
+				date="$year-$month-$day"
+				today=`date +%Y-%m-%d`
+				date=`date --date="$date" +%s`
+				today=`date --date="$today" +%s`
+				if [[ $date -le $today ]]; then
+					task=$(echo $l | sed "s/$datesave//g" | sed -e 's/^-//g' -e 's/[[:space:]]*$//')
+					regex='#\w+((-)?\w+)+(-?)'
+					tags=()
+					for t in ${task[@]}; do
+						if [[ $t =~ $regex ]]; then
+							tags+=($t)
+						fi
+					done
+					for tag in ${tags[@]}; do
+						task=$(echo $task | sed "s/$tag//")
+					done
+					task=$( echo $task | sed -e 's/[[:space:]]*$//')
+					echo "$task: due on $month/$day/$year"
+					echo '--------'
+				fi
 			fi
-			x=$((x+1))
+			filekey=$((x+1))
 		done
 	done
 
