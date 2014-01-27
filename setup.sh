b433337ae4ee387ba31d62f82c8dd7e3987a665d
Matthew Davies <daviesgeek@icloud.com>
2014-01-26 19:47:01 -0800

    Added notification command to setup Still needs to check/install if the command is installed

--- setup.sh
+++ setup.sh
@@ -52,7 +52,55 @@ while true; do
 	fi
 done
 
+#Set notification command
+if [[ ! $cmd == '' ]]; then
+	cmdSet='set'
+	cmdText="currently set to $cmdSet"
+else
+	if [[ `uname` == 'Linux' ]]; then
+		cmdOption='1) notify-send (default)\n2) growl (not supported yet)'
+	elif [[ `uname` == 'Darwin' ]]; then
+		cmdOption='1) terminal-notifier (default)\n2) growl-notify'
+	fi
+fi
+
+echo -e "What notification command do you want $NAME to use?\nSee the readme for more info.\n$cmdOption\n:\c"
+read cmdRet
+
+while true; do
+	if [[ `uname` == 'Linux' ]]; then
+		if [[ $cmdRet == '' ]]; then
+			cmdRet='notify-send'
+			break
+		elif [[ $cmdRet == '1' ]]; then
+			cmdRet='notify-send'
+			break
+		elif [[ $cmdRet == '2' ]]; then
+			echo "growl isn't supported yet on Linux. Please use notify-send instead (press enter)"
+			read cmdRet
+		else
+			echo -e "'$cmdRet' is not a valid option. Please try again.\n:\c"
+			read cmdRet
+		fi
+	elif [[ `uname` == 'Darwin' ]]; then
+		if [[ $cmdRet == '' ]]; then
+			cmdRet='terminal-notifier'
+			break
+		elif [[ $cmdRet == '1' ]]; then
+			cmdRet='terminal-notifier'
+			break
+		elif [[ $cmdRet == '2' ]]; then
+			cmdRet='growl-notify'
+			break
+		else
+			echo -e "'$cmdRet' is not a valid option. Please try again.\n:\c"
+			read cmdRet
+		fi
+	fi	
+done
+
 mkdir -p ${HOME}/.$NAME
 echo 'check='\"$checkRet\" >> "$SETTINGS"
 echo 'location='\"$locRet\" >> "$SETTINGS"
+echo 'cmd='\"$cmdRet\" >> "$SETTINGS"
 echo "Done setting up. Please re-run $NAME without any flags to start it in the background."
